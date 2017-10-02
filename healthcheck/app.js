b195d51087087232d6d5c4509bfe6f0614b79413
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2017-10-01 19:03:14 -0700

    updated healthcheck with ability to restart aws instance

--- healthcheck/app.js
+++ healthcheck/app.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 const spawn = require('child_process').spawn;
 const moment = require('moment'); 
 
-require('dotenv').config()
+require('dotenv').config();
 
 //returns mathematical modulus
 const mod = (n, m) => ( m < Infinity )? ((n % m) + m) % m : n ;
@@ -21,20 +21,22 @@ const near = (n, x, b, wrap=Infinity) => {
 		valid.push(mod(value, wrap));
 	});
 	return _.includes(valid, n);
-}
+};
 
 const uri = 'http://ec2-35-167-51-118.us-west-2.compute.amazonaws.com';
 
-const auth = { 
+//load all the env variables
+const env = {
+	awsInstanceID: process.env.AWS_INSTANCE_ID,
 	user: process.env.SENDGRID_USER,
 	pass: process.env.SENDGRID_PASSWORD,
-}; 
+};
 
 const envErrors = [];
-_.forIn(auth, (value, key) => {
+_.forIn(env, (value, key) => {
 	if(!value){
-		envErrors.push(`Enviroment variable '${key}' was undefined but is marked as required.`);
-	} 
+		envErrors.push(`Environment variable '${key}' was undefined but is marked as required.`);
+	}
 });
 
 if(envErrors.length){
@@ -43,9 +45,14 @@ if(envErrors.length){
 	return;
 }
 
+const auth = { 
+	user: env.user,
+	pass: env.pass,
+};
+
 const smtpTransport = nodemailer.createTransport({
 	service: 'SendGrid',
-	auth: auth,
+			auth: auth,
 }); 
 
 runScript = (scriptPath, subject, message) => {
@@ -75,10 +82,10 @@ runScript = (scriptPath, subject, message) => {
 					subject: subject,
 					html: message, 
 					attachments: [
-						{
-							filename: 'log.txt',
-							content: new Buffer(output, 'utf-8'),
-						}
+					{
+						filename: 'log.txt',
+						content: new Buffer(output, 'utf-8'),
+					}
 					]
 				}; 
 
@@ -87,71 +94,144 @@ runScript = (scriptPath, subject, message) => {
 				});
 			}
 	]);
-} 
+};
 
-sendEmail = (subject, message) => { 
+runAWS = (args, callback) => {
+	async.waterfall([
+			(done) => {
+				try{
+					const p = spawn('aws',args);
+					let output = '';
+					p.stdout.on('data', function(data) {
+						output += data;
+					});
+					p.on('error', function(err) {
+						done(null, null);
+					});
+					p.on('close' , function(code){
+						done(null, output);
+					});
+				} catch (err) {
+					done(null, null);
+				}
+			},
+			(output, done) => {
+				callback(output);
+				done(null, 'done');
+			}
+	]);
+};
+
+runProcess = (process, args, success, fail) => {
+	async.waterfall([
+			(done) => {
+				try{
+					const p = spawn(process, args);
+					let output = '';
+					p.stdout.on('data', function(data) {
+						output += data;
+					});
+					p.on('error', function(err) {
+						done(null, 'errored', err);
+					});
+					p.on('close' , function(code){
+						done(null, output, null);
+					});
+				} catch (err) {
+					done(null, 'errored', err);
+				}
+			},
+		(output, error, done) => {
+			fail(error);
+			success(output);
+			done(null, 'done');
+		}
+	]);
+};
+
+
+
+sendEmail = (subject, message, output) => {
 
 	const mailOptions = {
 		to: 'michaelsenpatrick@gmail.com',
 		from: 'servercheck@livebasketball.com',
 		subject: subject,
 		html: message,
-	}; 
+	};
+
+	if(output){
+		mailOptions.attachments = [
+		{
+			filename: 'log.txt',
+				content: new Buffer(output, 'utf-8'),
+		}];
+	}
 
 	smtpTransport.sendMail( mailOptions, (err)=>{
-		console.log('err', err);
+		done(err,'done');
 	});
-}
+};
 
 fetchData = () => {
 	return fetch(uri + '/livebasketball/leagues', {
 		method: 'GET',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json',
-		},
+				 headers: {
+					 'Accept': 'application/json',
+				 'Content-Type': 'application/json',
+				 },
 	});
-} 
+};
 
 console.log("App started");
 
 let serverUp = true;
 let serverInSync = true;
+let instanceOnline = true;
 
 healthCheck = () => { 
 	console.log(`Doing health check. Server status is ${(serverUp)? 'ok':'down'}`);
 	console.log(`Doing health check. Server is ${(serverInSync)? 'in sync':'out of sync'}`);
+	console.log(`Doing health check. Instance is ${(instanceOnline)? 'online':'offline'}`);
 	fetchData()
-	.then( res => {
-		if(res.ok){
-			if(!serverUp){
-				sendEmail('Server Rest Back Online', `
-					You are receving this email because the server
-					rest service stopped and was successfully restarted
-					automatically.
-					`);
+		.then( res => {
+			if(res.ok){
+				if(!serverUp){
+					sendEmail('Server Rest Back Online', `
+						You are receving this email because the server
+						rest service stopped and was successfully restarted
+						automatically.
+						`);
+				}
+				if(!instanceOnline){
+					sendEmail('Server Instance Back Online', `
+						You are receving this email because the server
+						aws instance stopped and was successfully restarted
+						automatically.
+						`);
+				}
+				serverUp = true;
+				instanceOnline = true;
+				return res.json();
 			}
-			serverUp = true;
-			return res.json();
-		}
-		else{
-			if(serverUp){
-				runScript('print_logs.sh', 'Server Rest Service Stopped', `
-					You are receiving this message because the server rest
-					service has stopped: 
-					Server status was ${res.status}
-					`
-					);
-				runScript('restart_rest.sh', 'Server Rest Restart Attempted', `
-					You are receiving this message because the rest
-					has attempted an automatic restart. See the attached
-					output
-					`
-					);
-			} 
-			serverUp = false;
-		}
-	})
+			else{
+				if(serverUp){
+					runScript('print_logs.sh', 'Server Rest Service Stopped', `
+						You are receiving this message because the server rest
+						service has stopped: 
+						Server status was ${res.status}
+						`
+						);
+					runScript('restart_rest.sh', 'Server Rest Restart Attempted', `
+						You are receiving this message because the rest
+						has attempted an automatic restart. See the attached
+						output
+						`
+						);
+				} 
+				serverUp = false;
+			}
+		})
 	.then( data => {
 		if(data && data.leagues){
 			const timestamp = _.find(data.leagues, (value, key) => {
@@ -191,8 +271,51 @@ healthCheck = () => {
 		}
 	})
 	.catch( err => {
-		console.log(err);
-	}); 
-} 
+		if(err) {
+			if(instanceOnline){
+				instanceOnline = false;
+				sendEmail('Server Instance Stopped',
+					`
+					You are receiving this message because the ec2 instance
+					stopped. See attached log.
+					`,
+					JSON.stringify(err)
+				); 
+				runProcess('aws', [
+						'ec2',
+						'reboot-instances',
+						'--instance-ids',
+						env.awsInstanceID,
+					],
+					(output) => {
+						let message = 'failed. See attached log';
+						if(!output){
+							 message = 'succeeded';
+							 output = message;
+						}
+				    sendEmail('Server Instance Restart Attempted',
+					    `
+					    You are receiving this message because the ec2 instance
+					    stopped and has attempted an automatic restart which 
+							${message}.
+					    `,
+							output 
+						);
+					},
+					(error) => { 
+				    sendEmail('Server Instance Restart Attempted',
+					    `
+					    You are receiving this message because the ec2 instance
+					    stopped and has attempted an automatic restart which likely
+							failed. See attached log.
+					    `,
+					    error
+						);
+					}
+				);
+			}
+		} 
+	});
+};
 
-const healthCheckInterval = setInterval(healthCheck, 5000); 
+const healthCheckInterval = setInterval(healthCheck, 5000);
